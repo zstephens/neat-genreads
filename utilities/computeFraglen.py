581029a43811b4adb0db46f76f45717594c03dc6
Zachary Stephens <zach@Zacharys-MacBook-Air.local>
2016-02-06 23:31:13 -0600

    adding fragment length computation script

--- utilities/computeFraglen.py
+++ utilities/computeFraglen.py
@@ -2,6 +2,18 @@ import fileinput
 import cPickle as pickle
 import numpy as np
 
+def quick_median(countDict):
+	midPoint = sum(countDict.values())/2
+	mySum    = 0
+	myInd    = 0
+	sk       = sorted(countDict.keys())
+	while mySum < midPoint:
+		mySum += countDict[sk[myInd]]
+		if mySum >= midPoint:
+			break
+		myInd += 1
+	return myInd
+
 all_tlens = {}
 
 PRINT_EVERY = 100000
@@ -20,9 +32,9 @@ for line in fileinput.input():
 			all_tlens[myTlen] += 1
 			i += 1
 			if i%PRINT_EVERY == 0:
-				print '---',i
-				for k in sorted(all_tlens.keys()):
-					print k, all_tlens[k]
+				print '---',i, quick_median(all_tlens)
+				#for k in sorted(all_tlens.keys()):
+				#	print k, all_tlens[k]
 
 for k in sorted(all_tlens.keys()):
 	print k, all_tlens[k]
