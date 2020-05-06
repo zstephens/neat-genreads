fce987409603cf26e26ddcf9fe80de7be4363cd2
Joshua Allen <joshfactorial@pop-os.localdomain>
2020-05-06 11:44:20 -0500

    trying out seqio for reading the fasta in computeGC.py

--- utilities/computeGC.py
+++ utilities/computeGC.py
@@ -15,99 +15,118 @@ import time
 import argparse
 import numpy as np
 import pickle
+from Bio import SeqIO
 
-parser = argparse.ArgumentParser(description='computeGC.py')
-parser.add_argument('-i', type=str, required=True, metavar='<str>', help="* input.genomecov")
-parser.add_argument('-r', type=str, required=True, metavar='<str>', help="* reference.fa")
-parser.add_argument('-o', type=str, required=True, metavar='<str>', help="* output.p")
-parser.add_argument('-w', type=int, required=False, metavar='<int>', help="sliding window length [50]", default=50)
-args = parser.parse_args()
 
-(IN_GCB, REF_FILE, WINDOW_SIZE, OUT_P) = (args.i, args.r, args.w, args.o)
+def process_fasta(file: str) -> dict:
+    ref_dict = {}
 
-GC_BINS = {n: [] for n in range(WINDOW_SIZE + 1)}
+    try:
+        ref_dict = SeqIO.to_dict(SeqIO.parse(file, 'fasta'))
+        # ref_dict = {rec.id: rec.seq for rec in SeqIO.parse(file, "fasta")}
+    except UnicodeDecodeError:
+        print("Input file incorrect: -r should specify the reference fasta")
+        exit(1)
 
-print('reading ref...')
-allRefs = {}
-f = open(REF_FILE, 'r')
-for line in f:
-    if line[0] == '>':
-        refName = line.strip()[1:]
-        allRefs[refName] = []
-        print(refName)
-    # if refName == 'chr2':
-    #	break
-    else:
-        allRefs[refName].append(line.strip())
-f.close()
+    if not ref_dict:
+        print("Input file incorrect: -r should specify the reference fasta")
+        exit(1)
 
-print('capitalizing ref...')
-for k in sorted(allRefs.keys()):
-    print(k)
-    allRefs[k] = ''.join(allRefs[k])
-    allRefs[k] = allRefs[k].upper()
+    return ref_dict
 
-print('reading genomecov file...')
-tt = time.time()
-f = open(IN_GCB, 'r')
-currentLine = 0
-currentRef = None
-currentCov = 0
-linesProcessed = 0
-PRINT_EVERY = 1000000
-STOP_AFTER = 1000000
-for line in f:
-    splt = line.strip().split('\t')
-    if linesProcessed % PRINT_EVERY == 0:
-        print(linesProcessed)
-    linesProcessed += 1
 
-    # if linesProcessed > STOP_AFTER:
-    #	break
+def capitalize_ref(input_dict: dict) -> dict:
+    for k in sorted(input_dict.keys()):
+        print("Capitalizing " + k)
+        input_dict[k] = ''.join(input_dict[k])
+        input_dict[k] = input_dict[k].upper()
 
-    if currentLine == 0:
-        currentRef = splt[0]
-        sPos = int(splt[1]) - 1
 
-    if currentRef not in allRefs:
-        continue
+def main():
+    parser = argparse.ArgumentParser(description='computeGC.py')
+    parser.add_argument('-i', type=str, required=True, metavar='input', help="input.genomecov")
+    parser.add_argument('-r', type=str, required=True, metavar='reference', help="reference.fasta")
+    parser.add_argument('-o', type=str, required=True, metavar='output prefix',
+                        help="prefix for output (/path/to/output)")
+    parser.add_argument('-w', type=int, required=False, metavar='sliding window',
+                        help="sliding window length [50]", default=50)
+    args = parser.parse_args()
 
-    currentLine += 1
-    currentCov += float(splt[2])
+    (IN_GCB, REF_FILE, WINDOW_SIZE, OUT_P) = (args.i, args.r, args.w, args.o)
 
-    if currentLine == WINDOW_SIZE:
-        currentLine = 0
-        seq = allRefs[currentRef][sPos:sPos + WINDOW_SIZE]
-        if 'N' not in seq:
-            gc_count = seq.count('G') + seq.count('C')
-            GC_BINS[gc_count].append(currentCov)
-        currentCov = 0
+    GC_BINS = {n: [] for n in range(WINDOW_SIZE + 1)}
 
-f.close()
+    print('reading ref...')
+    allrefs = process_fasta(REF_FILE)
 
-runningTot = 0
-allMean = 0.0
-for k in sorted(GC_BINS.keys()):
-    if len(GC_BINS[k]) == 0:
-        print('{0:0.2%}'.format(k / float(WINDOW_SIZE)), 0.0, 0)
-        GC_BINS[k] = 0
-    else:
-        myMean = np.mean(GC_BINS[k])
-        myLen = len(GC_BINS[k])
-        print('{0:0.2%}'.format(k / float(WINDOW_SIZE)), myMean, myLen)
-        allMean += myMean * myLen
-        runningTot += myLen
-        GC_BINS[k] = myMean
+    print('capitalizing ref...')
+    allrefs = capitalize_ref(allrefs)
 
-avgCov = allMean / float(runningTot)
-print('AVERAGE COVERAGE =', avgCov)
+    print('reading genomecov file...')
+    tt = time.time()
+    f = open(IN_GCB, 'r')
+    currentLine = 0
+    currentRef = None
+    currentCov = 0
+    linesProcessed = 0
+    PRINT_EVERY = 1000000
+    STOP_AFTER = 1000000
+    for line in f:
+        splt = line.strip().split('\t')
+        if linesProcessed % PRINT_EVERY == 0:
+            print(linesProcessed)
+        linesProcessed += 1
 
-y_out = []
-for k in sorted(GC_BINS.keys()):
-    GC_BINS[k] /= avgCov
-    y_out.append(GC_BINS[k])
+        # if linesProcessed > STOP_AFTER:
+        #	break
 
-print('saving model...')
-pickle.dump([range(WINDOW_SIZE + 1), y_out], open(OUT_P, 'wb'))
+        if currentLine == 0:
+            currentRef = splt[0]
+            sPos = int(splt[1]) - 1
 
-print(time.time() - tt, '(sec)')
+        if currentRef not in allrefs:
+            continue
+
+        currentLine += 1
+        currentCov += float(splt[2])
+
+        if currentLine == WINDOW_SIZE:
+            currentLine = 0
+            seq = allrefs[currentRef][sPos:sPos + WINDOW_SIZE]
+            if 'N' not in seq:
+                gc_count = seq.count('G') + seq.count('C')
+                GC_BINS[gc_count].append(currentCov)
+            currentCov = 0
+
+    f.close()
+
+    runningTot = 0
+    allMean = 0.0
+    for k in sorted(GC_BINS.keys()):
+        if len(GC_BINS[k]) == 0:
+            print('{0:0.2%}'.format(k / float(WINDOW_SIZE)), 0.0, 0)
+            GC_BINS[k] = 0
+        else:
+            myMean = np.mean(GC_BINS[k])
+            myLen = len(GC_BINS[k])
+            print('{0:0.2%}'.format(k / float(WINDOW_SIZE)), myMean, myLen)
+            allMean += myMean * myLen
+            runningTot += myLen
+            GC_BINS[k] = myMean
+
+    avgCov = allMean / float(runningTot)
+    print('AVERAGE COVERAGE =', avgCov)
+
+    y_out = []
+    for k in sorted(GC_BINS.keys()):
+        GC_BINS[k] /= avgCov
+        y_out.append(GC_BINS[k])
+
+    print('saving model...')
+    pickle.dump([range(WINDOW_SIZE + 1), y_out], open(OUT_P, 'wb'))
+
+    print(time.time() - tt, '(sec)')
+
+
+if __name__ == "__main__":
+    main()
