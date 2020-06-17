9355c2e72130db7ef9e8217ae6c0d14af1d194f7
meridith-e <66380709+meridith-e@users.noreply.github.com>
2020-06-17 15:09:28 -0500

    Update compute_gc.py

--- utilities/compute_gc.py
+++ utilities/compute_gc.py
@@ -43,7 +43,15 @@ def process_fasta(file: str) -> dict:
     return ref_dict
 
 
-def process_genomecov(file: str, ref_dict: dict, window) -> dict:
+def process_genomecov(file: str, ref_dict: dict, window: int) -> dict:
+    """
+    Takes a genomecov file and converts it into a dictionary made up of 'window' sized sections 
+    that record the number of GCs and the coverage measure for each section.
+    :param file: path to a genomecov file
+    :param ref_dict: dictionary created from using the process_fasta function
+    :param window: Length of each section of base pairs to count in the reference dictionary
+    :return: dictionary form of genomecov file based on window size and ref_dict data
+    """
     gc_bins = {n: [] for n in range(window + 1)}
 
     # variables needed to parse coverage file
@@ -51,7 +59,6 @@ def process_genomecov(file: str, ref_dict: dict, window) -> dict:
     current_ref = None
     current_cov = 0
     lines_processed = 0
-    print_every = 1000000
 
     f = open(file, 'r')
     for line in f:
@@ -59,7 +66,7 @@ def process_genomecov(file: str, ref_dict: dict, window) -> dict:
         lines_processed += 1
         if current_line == 0:
             current_ref = splt[0]
-            sPos = int(splt[1]) - 1
+            current_pos = int(splt[1]) - 1
 
         if current_ref not in ref_dict:
             continue
@@ -69,7 +76,7 @@ def process_genomecov(file: str, ref_dict: dict, window) -> dict:
 
         if current_line == window:
             current_line = 0
-            seq = str(ref_dict[current_ref][sPos:sPos + window])
+            seq = str(ref_dict[current_ref][current_pos:current_pos + window])
             if 'N' not in seq:
                 gc_count = seq.count('G') + seq.count('C')
                 gc_bins[gc_count].append(current_cov)
@@ -80,6 +87,28 @@ def process_genomecov(file: str, ref_dict: dict, window) -> dict:
 
 
 def calculate_coverage(bin_dict: dict, window: int) -> float:
+    """
+    Takes the dictionary created in process_genomecov and finds the average coverage value.
+    Also ouputs the average coverage value for each window, along with the number of entries in that window.
+    :param bin_dict: dictionary created from using the process_genomecov function
+    :param window: Length of each section of base pairs to count, 
+                   should be the same as the window value in process_genomecov
+    :return: Average coverage value for the whole sample, along with average coverage values for each window.
+    
+
+    Parameters
+    ----------
+    bin_dict : dict
+        DESCRIPTION.
+    window : int
+        DESCRIPTION.
+
+    Returns
+    -------
+    float
+        DESCRIPTION.
+
+    """
     running_total = 0
     all_mean = 0.0
     for k in sorted(bin_dict.keys()):
@@ -87,12 +116,12 @@ def calculate_coverage(bin_dict: dict, window: int) -> float:
             print('{0:0.2%}'.format(k / float(window)), 0.0, 0)
             bin_dict[k] = 0
         else:
-            myMean = np.mean(bin_dict[k])
-            myLen = len(bin_dict[k])
-            print('{0:0.2%}'.format(k / float(window)), myMean, myLen)
-            all_mean += myMean * myLen
-            running_total += myLen
-            bin_dict[k] = myMean
+            my_mean = np.mean(bin_dict[k])
+            my_len = len(bin_dict[k])
+            print('{0:0.2%}'.format(k / float(window)), my_mean, my_len)
+            all_mean += my_mean * my_len
+            running_total += my_len
+            bin_dict[k] = my_mean
 
     return all_mean / float(running_total)
 
@@ -101,8 +130,8 @@ def main():
     """
     Reads in arguments and processes the inputs to a GC count for the sequence.
     Parameters:
-        -i is the genome coverage input file
-        -r is the reference file
+        -i is the genome coverage input file (genomecov)
+        -r is the reference file (fasta)
         -o is the prefix for the output
         -w is the sliding window length. The default is 50, but you can declare any reasonable integer
 
