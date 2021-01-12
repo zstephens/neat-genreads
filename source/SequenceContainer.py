0edbd0fa93a030909879c32dee3deb43596d9589
Joshua Allen <jallen17@illinois.edu>
2021-01-11 19:12:22 -0600

    found another bug

--- source/SequenceContainer.py
+++ source/SequenceContainer.py
@@ -620,7 +620,7 @@ class SequenceContainer:
                     elif indel_length < 0:
                         cigar_to_insert = CigarString(str(abs(indel_length)) + 'D1M')
                         original_string = copy.deepcopy(temp_symbol_string)
-                        temp_symbol_string.insert_cigar_element(v_pos + 1, cigar_to_insert)
+                        temp_symbol_string.insert_cigar_element(v_pos + 1, cigar_to_insert, 1)
                         # TODO Delete commented out lines once CigarString works 100%
                         original_list = copy.deepcopy(temp_symbol_string2)
                         temp_symbol_string2[v_pos + 1] = 'D' * abs(indel_length) + 'M'
